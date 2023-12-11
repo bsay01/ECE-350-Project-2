eaf7a4c95966cf7369a39e49a5072ac76012a25d
Benjamin Say <benbsay@gmail.com>
2023-12-11 03:29:22 -0800

    config Dockerfile

--- start_container.sh
+++ start_container.sh
@@ -1,5 +1,5 @@
 # this starts the container that runs the docker container
 
-docker run -d --mount source=rtl_433_output,target=/src/data ece350project2:latest
+docker run -d --privileged --mount source=rtl_433_output,target=/src/data ece350project2:latest
 
 # docker system prune -a -f --volumes
