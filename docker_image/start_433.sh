c453fd446a77638636b6ccb2532b27f82b1415fa
Benjamin Say <benbsay@gmail.com>
2023-12-11 04:46:44 -0800

    add manual start

--- docker_image/start_433.sh
+++ docker_image/start_433.sh
@@ -1,6 +1,8 @@
 # script to run rtl_433 with correct flags (RUNS AT CONTAINER STARTUP)
 
-#sudo rtl_433 -s 2400000 -Y autolevel -Y ampest -Y squelch -H 30 -f 315000000 -f 433920000 -f 868000000 -f 915000000
+#mkdir -p data
 
-sudo mkdir -p data
-rtl_433 -F csv:data/log.csv
+#sudo rtl_433 -s 2400000 -Y autolevel -Y ampest -Y squelch -H 30 -f 315000000 -f 433920000 -f 868000000 -f 915000000
+#rtl_433 -F csv:data/log.csv
+
+echo "started!"
