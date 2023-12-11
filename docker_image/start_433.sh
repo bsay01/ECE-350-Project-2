8264e52605c198cf8805ff4252e033a68f971c77
Benjamin Say <benbsay@gmail.com>
2023-12-11 05:22:29 -0800

    cleanup - finally working

--- docker_image/start_433.sh
+++ docker_image/start_433.sh
@@ -1,9 +1,7 @@
 # script to run rtl_433 with correct flags (RUNS AT CONTAINER STARTUP)
 
-#mkdir -p data
+echo "starting!"
 
 #sudo rtl_433 -s 2400000 -Y autolevel -Y ampest -Y squelch -H 30 -f 315000000 -f 433920000 -f 868000000 -f 915000000
-#rtl_433 -F csv:data/log.csv
 
-echo "starting!"
 rtl_433 -F csv:data/log.csv
--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,10 @@
 # this starts the container that runs the docker container
 
+# to use a volume: (docker managed)
 #docker run -d --privileged --mount source=rtl_433_output,target=/src/data ece350project2:latest
+
+# to use a mind mount: (a directory in the file system)
 docker run -d --privileged --mount type=bind,source=/home/bsay/Documents/data,target=/src/data ece350project2:latest
 
+# sometimes best to remove stuff after
 # docker system prune -a -f --volumes
