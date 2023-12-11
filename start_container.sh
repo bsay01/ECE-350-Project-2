fa546ce3777074c13ed704e8b56b8cdb51524ee7
Benjamin Say <benbsay@gmail.com>
2023-12-11 06:15:29 -0800

    store data in repo

--- start_container.sh
+++ start_container.sh
@@ -4,7 +4,7 @@
 #docker run -d --privileged --mount source=rtl_433_output,target=/src/data ece350project2:latest
 
 # to use a mind mount: (a directory in the file system)
-docker run -d --privileged --mount type=bind,source=/home/bsay/Documents/data,target=/src/data ece350project2:latest
+docker run -d --privileged --mount type=bind,source=./data,target=/src/data ece350project2:latest
 
 # sometimes best to remove stuff after
 # docker system prune -a -f --volumes
