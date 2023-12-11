9b9714f7e64054b3dfaff09484e6b483ecaabb99
Benjamin Say <benbsay@gmail.com>
2023-12-11 14:38:51 -0800

    back to csv data and add analysis

--- docker_image/start_433.sh
+++ docker_image/start_433.sh
@@ -5,4 +5,4 @@ echo "starting!"
 #sudo rtl_433 -s 2400000 -Y autolevel -Y ampest -Y squelch -H 30 -f 315000000 -f 433920000 -f 868000000 -f 915000000
 
 #rtl_433 -F csv:data/log.csv
-rtl_433 -F json:data/log.json -C si
+rtl_433 -F csv:data/log.csv -C si
