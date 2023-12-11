42e7fb9fc6d28008cec8d79b4ee442339a45153b
Benjamin Say <benbsay@gmail.com>
2023-12-11 02:23:42 -0800

    enter container and run startup script manually now

--- docker_image/start_433.sh
+++ docker_image/start_433.sh
@@ -3,4 +3,4 @@
 #sudo rtl_433 -s 2400000 -Y autolevel -Y ampest -Y squelch -H 30 -f 315000000 -f 433920000 -f 868000000 -f 915000000
 
 sudo mkdir -p data
-sudo rtl_433 -F csv:data/log.csv
+#sudo rtl_433 -F csv:data/log.csv
