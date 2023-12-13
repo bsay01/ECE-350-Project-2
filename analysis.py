e9d41d14eaa64d06a824831cd0eb8f9453f3bafd
Benjamin Say <benbsay@gmail.com>
2023-12-12 21:33:50 -0800

    made analysis better

--- analysis.py
+++ analysis.py
@@ -1,57 +1,67 @@
 import os, pandas
 
-# returns a list of relative paths to all csv files in the working folder
+print("\nLooking for files...\n")
+
+# returns a list of relative paths to all csv files in the given directory
 def checkDirForCSV(dir_name = "", indent = 0):
     print("\t"*indent + "Files found in \"" + dir_name + "\" folder:")
-    indent += 1
+    indent = indent + 1
     files = []
     for entry in os.listdir(dir_name):
         if ".csv" in entry: # for a csv file
             print("\t"*indent + dir_name + "\\" + entry)
             files.append(dir_name + "\\" + entry)
-        elif "." not in entry: # for a subfolder
+        elif "." not in entry: # for a subdirectory
             files.extend(checkDirForCSV(dir_name + "\\" + entry, indent))
+    files.sort()
     return files
 
-print("\nAnalyzing files in the \"data\" folder\n")
+files = checkDirForCSV(".\\data") # check for csv files
 
-files = checkDirForCSV(".\\data") # check for csv files in the current directory
-files.sort()
+print("\nCombining files...\n")
 
-df = pandas.read_csv(files[0])
+complete_dataframe = pandas.DataFrame()
 for file in files:
     print("Adding file \"" + file + "\"...")
-    df = pandas.concat([df, pandas.read_csv(file, dtype='unicode')]).drop_duplicates()
+    complete_dataframe = pandas.concat([complete_dataframe, pandas.read_csv(file, dtype='unicode')]).drop_duplicates()
+complete_dataframe.sort_values('time')
 
-print("\nfixing \"NaN\" values\n")
-nan_value = float("NaN")
-df = df.replace("", nan_value)
-df = df.dropna(how = 'all', axis = 1)
+print("\nfixing \"NaN\" values and removing empty columns")
 
-df.sort_values('time')
+complete_dataframe.replace("", float("NaN"), inplace=True)
+complete_dataframe.dropna(how = 'all', axis = 1, inplace=True)
 
-print("\nCreating file containing combined data\n")
-df.to_csv(".\\data_processed\\0-data_processed.csv")
+print("\nCreating file containing combined data")
 
-device_names = []
-device = [0, 0]
-temp_df = df
+complete_dataframe.to_csv(".\\data_processed\\0-data_processed.csv")
 
-for model, id in zip(df['model'], df['id']):
+print("\nCreating files for individual devices...\n")
 
-    device = [model, id]
+unique_devices = []
+this_row_device = []
+temp_df = pandas.DataFrame()
 
-    if device not in device_names:
+print("Creating files for the following devices:\n")
+print("{m:^19}|{i:^19}".format(m="model name", i="device id"))
+print("---------------------------------------")
 
-        print("Extracting data for {model} : {id}".format(model=device[0], id=device[1]))
+for model, id in zip(complete_dataframe['model'], complete_dataframe['id']):
 
-        device_names.append(device)
+    this_row_device = [model, id]
 
-        temp_df = temp_df[temp_df['id'] == device[1]]
-        temp_df = df[df['model'] == device[0]]
+    if this_row_device not in unique_devices:
 
-        temp_df = temp_df.dropna(how = 'all', axis = 1)
+        unique_devices.append(this_row_device)
 
-        temp_df.to_csv(".\\data_processed\\{model}_{id}.csv".format(model=device[0], id=device[1]))
+        print("|{model:^18}|{id:^18}|".format(model=this_row_device[0], id=this_row_device[1]))
+
+        temp_df = complete_dataframe[complete_dataframe['id'] == this_row_device[1]]
+        temp_df = temp_df[temp_df['model'] == this_row_device[0]]
+
+        temp_df.dropna(how = 'all', axis = 1, inplace = True)
+
+        temp_df.to_csv(".\\data_processed\\{model}_{id}.csv".format(model=this_row_device[0], id=this_row_device[1]))
+
+print("---------------------------------------")
 
 print("\nData processing complete\n")
