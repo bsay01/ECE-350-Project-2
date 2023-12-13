2cafeb49d39ea61ba695c05ec9aafcac1c2a05a2
Benjamin Say <benbsay@gmail.com>
2023-12-12 17:25:15 -0800

    improve data analysis script

--- analysis.py
+++ analysis.py
@@ -1,6 +1,4 @@
-import os, csv, pandas, numpy
-
-### FUNCTIONS ###
+import os, pandas
 
 # returns a list of relative paths to all csv files in the working folder
 def checkDirForCSV(dir_name = "", indent = 0):
@@ -15,85 +13,45 @@ def checkDirForCSV(dir_name = "", indent = 0):
             files.extend(checkDirForCSV(dir_name + "\\" + entry, indent))
     return files
 
-# checks if a row of a parsed csv is empty. returns true if so.
-def csvRowEmpty(row):
-    isEmpty = True
-    for entry in row:
-        if entry != "": isEmpty = False
-    return isEmpty
+print("\nAnalyzing files in the \"data\" folder\n")
 
-### END FUNCTIONS ###
-
-print("\nFiles must be in the current folder or in subdirectories of the current folder.")
-print("All files must have the same headers. note that including a previous output of this file is fine.\n")
-
-# list to store files
 files = checkDirForCSV(".\\data") # check for csv files in the current directory
 files.sort()
-print
 
-allData = [] # will store the data as one giant table
-for file in files:
-    open_file = open(file)
-    print("Adding file \"" + file + "\"...")
-    for row in csv.reader(open_file, delimiter = ','):
-        if not csvRowEmpty(row) and row not in allData: allData.append(row)
-    open_file.close
-print
-
-### DATA LOADED INTO A SINGLE TABLE ###
-
-# manipulate data here.
-
-### OUTPUT TABLE AS A FILE ###
-
-finalFileName = "data_total.csv"
-
-#outputFile = open(".\\data\\" + finalFileName, mode="w", newline="")
-
-try:
-    outputFile = open(".\\data\\" + finalFileName, mode="w")
-except:
-    print("Error: could not create or write to output file.\nThis will throw if you have the file open.\n\nExiting...\n")
-    exit()
-
-print("Combining...\n")
-csv.writer(outputFile).writerows(allData)
-
-outputFile.close()
-
-#print(allData)
-#print
-
-print("Combined csv entitled \"" + finalFileName + "\"\n\nExiting...\n")
-
-df = pandas.read_csv(files[0], )
+df = pandas.read_csv(files[0])
 for file in files:
     print("Adding file \"" + file + "\"...")
-    df = pandas.concat([df, pandas.read_csv(file)]).drop_duplicates()
-print
+    df = pandas.concat([df, pandas.read_csv(file, dtype='unicode')]).drop_duplicates()
 
-df.dropna(how = 'all', axis = 1, inplace = True)
+print("\nfixing \"NaN\" values\n")
+nan_value = float("NaN")
+df = df.replace("", nan_value)
+df = df.dropna(how = 'all', axis = 1)
+
 df.sort_values('time')
+
+print("\nCreating file containing combined data\n")
+df.to_csv(".\\data_processed\\0-data_processed.csv")
 
 device_names = []
 device = [0, 0]
+temp_df = df
+
 for model, id in zip(df['model'], df['id']):
+
     device = [model, id]
+
     if device not in device_names:
+
+        print("Extracting data for {model} : {id}".format(model=device[0], id=device[1]))
+
         device_names.append(device)
 
-temp_df = df
-nan_value = numpy.float64("NaN")
-for device in device_names:
-    temp_df = df[df['model'] == device[0]]
-    temp_df = temp_df[temp_df['id'] == device[1]]
-    #print(temp_df)
-    temp_df.replace("", nan_value, inplace=True)
-    temp_df.dropna(how = 'all', axis = 1, inplace = True)
-    #print(temp_df)
-    temp_df.to_csv(".\\after_pandas\\{model}_{id}.csv".format(model=device[0], id=device[1]))
+        temp_df = temp_df[temp_df['id'] == device[1]]
+        temp_df = df[df['model'] == device[0]]
 
-df.to_csv(".\\after_pandas\\0-after_pandas.csv")
+        temp_df = temp_df.dropna(how = 'all', axis = 1)
 
-print(type(df['msg'][1]))
+        temp_df.to_csv(".\\data_processed\\{model}_{id}.csv".format(model=device[0], id=device[1]))
+
+print("\nData processing complete\n")
--- configure_docker_raspbian.sh
+++ configure_docker_raspbian.sh
@@ -45,4 +45,5 @@ sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
 # install python2 to use
-sudo apt install python-is-python2
+sudo apt install python-is-python3
+sudo pip install pandas
